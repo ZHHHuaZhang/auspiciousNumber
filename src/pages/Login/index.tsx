fa82863d6c8851ca8e4b45fbefd9d93c7902a368
ZHHHuaZhang <545219616@qq.com>
2024-09-29 17:05:11 +0800

    initial commit

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,44 @@
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import {
+  LoginForm,
+  PageContainer,
+  ProFormText,
+} from '@ant-design/pro-components';
+import { history } from '@umijs/max';
+import styles from './index.less';
+
+const Login: React.FC = () => {
+  return (
+    <PageContainer ghost>
+      <div className={styles.container}>
+        <LoginForm
+          onFinish={(values) => {
+            history.push('/home');
+            console.log('login', values);
+          }}
+        >
+          <>
+            <ProFormText
+              name="username"
+              fieldProps={{
+                size: 'large',
+                prefix: <UserOutlined className={'prefixIcon'} />,
+              }}
+              placeholder={'just clicking login button'}
+            />
+            <ProFormText.Password
+              name="password"
+              fieldProps={{
+                size: 'large',
+                prefix: <LockOutlined className={'prefixIcon'} />,
+              }}
+              placeholder={'you can get in'}
+            />
+          </>
+        </LoginForm>
+      </div>
+    </PageContainer>
+  );
+};
+
+export default Login;
